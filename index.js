e275fc7ee217003340c9d605aec94251952ccc20
Qijun Yu <force2008@gmail.com>
2023-10-10 21:44:11 +0800

    change localhost port

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors({
       if (ctx.url === '/upload') {
           return '*'; // 允许来自所有域名请求
       }
-      return 'http://localhost:8080'; //只允许http://localhost:8080这个域名的请求
+      return 'http://localhost:3000'; //只允许http://localhost:8080这个域名的请求
   },
   maxAge: 5, //指定本次预检请求的有效期，单位为秒。
   credentials: true, //是否允许发送Cookie
