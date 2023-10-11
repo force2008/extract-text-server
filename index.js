5298bbe174314b99e612d0a35d4d86e6d81692d3
Qijun Yu <force2008@gmail.com>
2023-10-11 08:09:42 +0800

    add allow headers

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.use(cors({
   maxAge: 5, //指定本次预检请求的有效期，单位为秒。
   credentials: true, //是否允许发送Cookie
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
-  allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
-  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept','x-requested-with'], //设置服务器支持的所有头信息字段
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], //设置获取其他自定义字段
 }));
 app.use(async (ctx, next)=>{
     ctx.state = {
